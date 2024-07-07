c0411773f29e66f6c04be5d5c820e63589a8977d
gerialkot <60850001+gerialkot@users.noreply.github.com>
2024-07-07 11:33:15 +0200

    Update Livelink_Gyro_ESP.ino
    bugfix

--- Livelink_Gyro_ESP.ino
+++ Livelink_Gyro_ESP.ino
@@ -7,9 +7,9 @@
 
 MPU6050 mpu6050(Wire);
 
-const char* ssid = "WIFI HALOZAT NEVE";
-const char* password = "JELSZO";
-const char* pc_ip = "FOGADO IP CIME";
+const char* ssid = "WIFI HÁLÓZAT NEVE";
+const char* password = "WIFI HÁLÓZAT JELSZAVA";
+const char* pc_ip = "FOGADÓ PC IP CÍME";
 const int pc_port = 8888;
 
 WiFiUDP udp;
@@ -104,6 +104,23 @@ const unsigned long kalmanCalibrationTime = 5000; // 5 másodperc kalibrálási 
 float zuptThreshold = 0.01; // ZUPT küszöbérték
 bool zuptEnabled = true;
 
+// Aluláteresztő szűrő paraméterei
+const int numReadings = 10; // A mozgóátlag minta száma
+float gyroPitchReadings[numReadings];
+float gyroRollReadings[numReadings];
+float gyroYawReadings[numReadings];
+
+int readIndex = 0;
+float totalPitch = 0;
+float totalRoll = 0;
+float totalYaw = 0;
+
+float averagePitch = 0;
+float averageRoll = 0;
+float averageYaw = 0;
+
+bool useLowPassFilter = true;
+
 void playDJISound() {
     tone(buzzerPin, NOTE_G5, 100);
     delay(150);
@@ -147,21 +164,6 @@ void playReadySound() {
     delay(200);
     noTone(buzzerPin);
 }
-
-const int numReadings = 10; // A mozgóátlag minta száma
-
-float gyroPitchReadings[numReadings];
-float gyroRollReadings[numReadings];
-float gyroYawReadings[numReadings];
-
-int readIndex = 0;
-float totalPitch = 0;
-float totalRoll = 0;
-float totalYaw = 0;
-
-float averagePitch = 0;
-float averageRoll = 0;
-float averageYaw = 0;
 
 void setup() {
     Serial.begin(115200);
@@ -210,28 +212,34 @@ void loop() {
     float gyroRoll = mpu6050.getGyroY();
     float gyroYaw = -mpu6050.getGyroZ(); // Invertáljuk a gyro yaw értéket
 
-    // Aluláteresztő szűrő alkalmazása
-    totalPitch = totalPitch - gyroPitchReadings[readIndex];
-    totalRoll = totalRoll - gyroRollReadings[readIndex];
-    totalYaw = totalYaw - gyroYawReadings[readIndex];
+    // Aluláteresztő szűrő alkalmazása, ha be van kapcsolva
+    if (useLowPassFilter) {
+        totalPitch = totalPitch - gyroPitchReadings[readIndex];
+        totalRoll = totalRoll - gyroRollReadings[readIndex];
+        totalYaw = totalYaw - gyroYawReadings[readIndex];
 
-    gyroPitchReadings[readIndex] = gyroPitch;
-    gyroRollReadings[readIndex] = gyroRoll;
-    gyroYawReadings[readIndex] = gyroYaw;
+        gyroPitchReadings[readIndex] = gyroPitch;
+        gyroRollReadings[readIndex] = gyroRoll;
+        gyroYawReadings[readIndex] = gyroYaw;
 
-    totalPitch = totalPitch + gyroPitch;
-    totalRoll = totalRoll + gyroRoll;
-    totalYaw = totalYaw + gyroYaw;
+        totalPitch = totalPitch + gyroPitch;
+        totalRoll = totalRoll + gyroRoll;
+        totalYaw = totalYaw + gyroYaw;
 
-    readIndex = readIndex + 1;
+        readIndex = readIndex + 1;
 
-    if (readIndex >= numReadings) {
-        readIndex = 0;
+        if (readIndex >= numReadings) {
+            readIndex = 0;
+        }
+
+        averagePitch = totalPitch / numReadings;
+        averageRoll = totalRoll / numReadings;
+        averageYaw = totalYaw / numReadings;
+    } else {
+        averagePitch = gyroPitch;
+        averageRoll = gyroRoll;
+        averageYaw = gyroYaw;
     }
-
-    averagePitch = totalPitch / numReadings;
-    averageRoll = totalRoll / numReadings;
-    averageYaw = totalYaw / numReadings;
 
     static unsigned long lastTime = 0;
     unsigned long now = micros();
@@ -267,131 +275,134 @@ void loop() {
     }
 }
 
-
 void sendFREEDData(float pitch, float roll, float yaw) {
-  uint8_t freedMessage[29] = {0xD1}; // Start byte
-  freedMessage[1] = 0x00; // Camera ID
+    uint8_t freedMessage[29] = {0xD1}; // Start byte
+    freedMessage[1] = 0x00; // Camera ID
 
-  int32_t pan = sendPitch ? static_cast<int32_t>(pitch * 32768) : 0;
-  int32_t tilt = sendYaw ? static_cast<int32_t>(yaw * 32768) : 0;
-  int32_t rollVal = sendRoll ? static_cast<int32_t>(roll * 32768) : 0;
+    int32_t pan = sendPitch ? static_cast<int32_t>(pitch * 32768) : 0;
+    int32_t tilt = sendYaw ? static_cast<int32_t>(yaw * 32768) : 0;
+    int32_t rollVal = sendRoll ? static_cast<int32_t>(roll * 32768) : 0;
 
-  freedMessage[2] = (tilt >> 16) & 0xFF;
-  freedMessage[3] = (tilt >> 8) & 0xFF;
-  freedMessage[4] = tilt & 0xFF;
-  freedMessage[5] = (pan >> 16) & 0xFF;
-  freedMessage[6] = (pan >> 8) & 0xFF;
-  freedMessage[7] = pan & 0xFF;
-  freedMessage[8] = (rollVal >> 16) & 0xFF;
-  freedMessage[9] = (rollVal >> 8) & 0xFF;
-  freedMessage[10] = rollVal & 0xFF;
+    freedMessage[2] = (tilt >> 16) & 0xFF;
+    freedMessage[3] = (tilt >> 8) & 0xFF;
+    freedMessage[4] = tilt & 0xFF;
+    freedMessage[5] = (pan >> 16) & 0xFF;
+    freedMessage[6] = (pan >> 8) & 0xFF;
+    freedMessage[7] = pan & 0xFF;
+    freedMessage[8] = (rollVal >> 16) & 0xFF;
+    freedMessage[9] = (rollVal >> 8) & 0xFF;
+    freedMessage[10] = rollVal & 0xFF;
 
-  // Position, Zoom, Focus data remains unchanged
+    // Position, Zoom, Focus data remains unchanged
 
-  uint8_t checksum = 0x40;
-  for (int i = 0; i < 26; i++) {
-    checksum -= freedMessage[i];
-  }
-  checksum %= 256;
-  freedMessage[26] = checksum;
+    uint8_t checksum = 0x40;
+    for (int i = 0; i < 26; i++) {
+        checksum -= freedMessage[i];
+    }
+    checksum %= 256;
+    freedMessage[26] = checksum;
 
-  freedMessage[27] = 0;
-  freedMessage[28] = 0;
+    freedMessage[27] = 0;
+    freedMessage[28] = 0;
 
-  udp.beginPacket(pc_ip, pc_port);
-  udp.write(freedMessage, sizeof(freedMessage));
-  udp.endPacket();
+    udp.beginPacket(pc_ip, pc_port);
+    udp.write(freedMessage, sizeof(freedMessage));
+    udp.endPacket();
 }
 
 void setupWebServer() {
-  server.on("/", HTTP_GET, handleRoot);
-  server.on("/update", HTTP_POST, handleUpdate);
-  server.on("/angles", HTTP_GET, handleAngles);
-  server.begin();
+    server.on("/", HTTP_GET, handleRoot);
+    server.on("/update", HTTP_POST, handleUpdate);
+    server.on("/angles", HTTP_GET, handleAngles);
+    server.begin();
 }
 
 void handleRoot() {
-  String html = "<html><body>";
-  html += "<h1>ESP32 MPU6050 VPCAM by Gerialkot</h1>";
-  html += "<form method='POST' action='/update'>";
-  html += "Kalman Strength Pitch: <input type='range' min='0.01' max='0.99' step='0.01' name='kalmanStrengthPitch' value='" + String(kalmanStrengthPitch) + "'><br>";
-  html += "Kalman Strength Roll: <input type='range' min='0.01' max='0.99' step='0.01' name='kalmanStrengthRoll' value='" + String(kalmanStrengthRoll) + "'><br>";
-  html += "Kalman Strength Yaw: <input type='range' min='0.01' max='0.99' step='0.01' name='kalmanStrengthYaw' value='" + String(kalmanStrengthYaw) + "'><br>";
-  html += "Send Pitch: <input type='checkbox' name='pitch' " + String(sendPitch ? "checked" : "") + "><br>";
-  html += "Send Roll: <input type='checkbox' name='roll' " + String(sendRoll ? "checked" : "") + "><br>";
-  html += "Send Yaw: <input type='checkbox' name='yaw' " + String(sendYaw ? "checked" : "") + "><br>";
-  html += "ZUPT Threshold: <input type='number' step='0.001' name='zuptThreshold' value='" + String(zuptThreshold) + "'><br>";
-  html += "Enable ZUPT: <input type='checkbox' name='zuptEnabled' " + String(zuptEnabled ? "checked" : "") + "><br>";
-  html += "<input type='submit' value='Update'>";
-  html += "</form>";
-  html += "<div id='angles'></div>";
-  html += "<script>"
-          "setInterval(function() {"
-          "fetch('/angles').then(response => response.json()).then(data => {"
-          "document.getElementById('angles').innerHTML = 'Pitch: ' + data.pitch + '<br>Roll: ' + data.roll + '<br>Yaw: ' + data.yaw;"
-          "});"
-          "}, 100);"
-          "</script>";
-  html += "</body></html>";
-  server.send(200, "text/html", html);
+    String html = "<html><body>";
+    html += "<h1>ESP32 MPU6050 VPCAM by Gerialkot</h1>";
+    html += "<form method='POST' action='/update'>";
+    html += "Kalman Alpha Pitch: <input type='range' min='0.01' max='0.99' step='0.01' name='kalmanStrengthPitch' value='" + String(kalmanStrengthPitch) + "'><br>";
+    html += "Kalman Alpha Roll: <input type='range' min='0.01' max='0.99' step='0.01' name='kalmanStrengthRoll' value='" + String(kalmanStrengthRoll) + "'><br>";
+    html += "Kalman Alpha Yaw: <input type='range' min='0.01' max='0.99' step='0.01' name='kalmanStrengthYaw' value='" + String(kalmanStrengthYaw) + "'><br>";
+    html += "Send Pitch: <input type='checkbox' name='pitch' " + String(sendPitch ? "checked" : "") + "><br>";
+    html += "Send Roll: <input type='checkbox' name='roll' " + String(sendRoll ? "checked" : "") + "><br>";
+    html += "Send Yaw: <input type='checkbox' name='yaw' " + String(sendYaw ? "checked" : "") + "><br>";
+    html += "ZUPT Threshold: <input type='number' step='0.001' name='zuptThreshold' value='" + String(zuptThreshold) + "'><br>";
+    html += "Enable ZUPT: <input type='checkbox' name='zuptEnabled' " + String(zuptEnabled ? "checked" : "") + "><br>";
+    html += "Use Low Pass Filter: <input type='checkbox' name='useLowPassFilter' " + String(useLowPassFilter ? "checked" : "") + "><br>";
+    html += "<input type='submit' value='Update'>";
+    html += "</form>";
+    html += "<div id='angles'></div>";
+    html += "<script>"
+            "setInterval(function() {"
+            "fetch('/angles').then(response => response.json()).then(data => {"
+            "document.getElementById('angles').innerHTML = 'Pitch: ' + data.pitch + '<br>Roll: ' + data.roll + '<br>Yaw: ' + data.yaw;"
+            "});"
+            "}, 100);"
+            "</script>";
+    html += "</body></html>";
+    server.send(200, "text/html", html);
 }
 
 void handleUpdate() {
-  if (server.hasArg("kalmanStrengthPitch")) {
-    kalmanStrengthPitch = server.arg("kalmanStrengthPitch").toFloat();
-    kalmanPitch.setRmeasure(1.0f / kalmanStrengthPitch);
-  }
-  if (server.hasArg("kalmanStrengthRoll")) {
-    kalmanStrengthRoll = server.arg("kalmanStrengthRoll").toFloat();
-    kalmanRoll.setRmeasure(1.0f / kalmanStrengthRoll);
-  }
-  if (server.hasArg("kalmanStrengthYaw")) {
-    kalmanStrengthYaw = server.arg("kalmanStrengthYaw").toFloat();
-    kalmanYaw.setRmeasure(1.0f / kalmanStrengthYaw);
-  }
-  if (server.hasArg("zuptThreshold")) {
-    zuptThreshold = server.arg("zuptThreshold").toFloat();
-  }
-  zuptEnabled = server.hasArg("zuptEnabled");
+    if (server.hasArg("kalmanStrengthPitch")) {
+        kalmanStrengthPitch = server.arg("kalmanStrengthPitch").toFloat();
+        kalmanPitch.setRmeasure(1.0f / kalmanStrengthPitch);
+    }
+    if (server.hasArg("kalmanStrengthRoll")) {
+        kalmanStrengthRoll = server.arg("kalmanStrengthRoll").toFloat();
+        kalmanRoll.setRmeasure(1.0f / kalmanStrengthRoll);
+    }
+    if (server.hasArg("kalmanStrengthYaw")) {
+        kalmanStrengthYaw = server.arg("kalmanStrengthYaw").toFloat();
+        kalmanYaw.setRmeasure(1.0f / kalmanStrengthYaw);
+    }
+    if (server.hasArg("zuptThreshold")) {
+        zuptThreshold = server.arg("zuptThreshold").toFloat();
+    }
+    zuptEnabled = server.hasArg("zuptEnabled");
+    useLowPassFilter = server.hasArg("useLowPassFilter");
 
-  sendPitch = server.hasArg("pitch");
-  sendRoll = server.hasArg("roll");
-  sendYaw = server.hasArg("yaw");
+    sendPitch = server.hasArg("pitch");
+    sendRoll = server.hasArg("roll");
+    sendYaw = server.hasArg("yaw");
 
-  saveSettings();
+    saveSettings();
 
-  server.sendHeader("Location", "/");
-  server.send(303);
+    server.sendHeader("Location", "/");
+    server.send(303);
 }
 
 void handleAngles() {
-  String json = "{\"pitch\":" + String(pitchFiltered) + ",\"roll\":" + String(rollFiltered) + ",\"yaw\":" + String(yawFiltered) + "}";
-  server.send(200, "application/json", json);
+    String json = "{\"pitch\":" + String(pitchFiltered) + ",\"roll\":" + String(rollFiltered) + ",\"yaw\":" + String(yawFiltered) + "}";
+    server.send(200, "application/json", json);
 }
 
 void loadSettings() {
-  EEPROM.get(0, kalmanStrengthPitch);
-  EEPROM.get(4, kalmanStrengthRoll);
-  EEPROM.get(8, kalmanStrengthYaw);
-  EEPROM.get(12, sendPitch);
-  EEPROM.get(13, sendRoll);
-  EEPROM.get(14, sendYaw);
-  EEPROM.get(15, zuptThreshold);
-  EEPROM.get(19, zuptEnabled);
+    EEPROM.get(0, kalmanStrengthPitch);
+    EEPROM.get(4, kalmanStrengthRoll);
+    EEPROM.get(8, kalmanStrengthYaw);
+    EEPROM.get(12, sendPitch);
+    EEPROM.get(13, sendRoll);
+    EEPROM.get(14, sendYaw);
+    EEPROM.get(15, zuptThreshold);
+    EEPROM.get(19, zuptEnabled);
+    EEPROM.get(20, useLowPassFilter);
 
-  kalmanPitch.setRmeasure(1.0f / kalmanStrengthPitch);
-  kalmanRoll.setRmeasure(1.0f / kalmanStrengthRoll);
-  kalmanYaw.setRmeasure(1.0f / kalmanStrengthYaw);
+    kalmanPitch.setRmeasure(1.0f / kalmanStrengthPitch);
+    kalmanRoll.setRmeasure(1.0f / kalmanStrengthRoll);
+    kalmanYaw.setRmeasure(1.0f / kalmanStrengthYaw);
 }
 
 void saveSettings() {
-  EEPROM.put(0, kalmanStrengthPitch);
-  EEPROM.put(4, kalmanStrengthRoll);
-  EEPROM.put(8, kalmanStrengthYaw);
-  EEPROM.put(12, sendPitch);
-  EEPROM.put(13, sendRoll);
-  EEPROM.put(14, sendYaw);
-  EEPROM.put(15, zuptThreshold);
-  EEPROM.put(19, zuptEnabled);
-  EEPROM.commit();
+    EEPROM.put(0, kalmanStrengthPitch);
+    EEPROM.put(4, kalmanStrengthRoll);
+    EEPROM.put(8, kalmanStrengthYaw);
+    EEPROM.put(12, sendPitch);
+    EEPROM.put(13, sendRoll);
+    EEPROM.put(14, sendYaw);
+    EEPROM.put(15, zuptThreshold);
+    EEPROM.put(19, zuptEnabled);
+    EEPROM.put(20, useLowPassFilter);
+    EEPROM.commit();
 }
